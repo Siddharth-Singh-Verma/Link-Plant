2335ada72bb39510ae089fa320e8314cf209df52
Siddharth Singh Verma <143976655+Siddharth-Singh-Verma@users.noreply.github.com>
2025-04-24 13:35:58 +0000

    uploading my project from replit

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd party,
+    "crispy_forms",
+    "crispy_tailwind",
+    
+    #local apps
+    'link_plant',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +133,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy form settings
+CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
+CRISPY_TEMPLATE_PACK = "tailwind"
---
+++ link_plant/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import  Profile, Link
+# Create your views here.
+
+class LinkListView(ListView):
+    model = Link # this will use the default template name: link_list.html
+    #(modelname_list.html)
+
+class LinkCreateView(CreateView):
+    # functional way : create forms.py file & form
+    # check if this was a post or get request
+    # return an empty form or or save the form data
+    # class based way : use CreateView ,it expects template model_form -> link_form.html
+    #(model_name_form.html)
+    #means it auto render that template so you create it
+    model = Link
+    fields = "__all__"
+    success_url = reverse_lazy('link-list')
+
+class LinkUpdateView(UpdateView):
+    # create a form
+    # check if a get request or a put request
+    # either render the form or update and save in our db
+    # use UpdateView ,it expects template model_form -> link_form.html same as create
+    model = Link
+    fields = ['text','url']
+    success_url = reverse_lazy('link-list')
+
+class LinkDeleteView(DeleteView):
+    # take in a id/pk of an object 
+    # query to db for that object
+    # check if it exists -> delete the object
+    # return some template or forward to user to some url
+    # use DeleteView: expects template model_confirm_delete -> link_confirm_delete.html
+    model = Link
+    success_url = reverse_lazy('link-list')
+    # form to submit to delete the item
+    # expect a template name model_confirm_delete.html
+    # link_confirm_delete.html
+
+def profile_view(request, profile_slug):
+    profile = get_object_or_404(Profile, slug=profile_slug)
+    links = profile.links.all()
+    context = {
+        "profile": profile,
+        "links": links
+    }
+    return render(request, 'link_plant/profile.html', context)
