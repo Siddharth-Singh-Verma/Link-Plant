556ce06aeb82e555671e9b64e9f3597f64b86625
Siddharth Singh Verma <143976655+Siddharth-Singh-Verma@users.noreply.github.com>
2025-04-24 20:09:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
- #!/usr/bin/env bash
- # exit on error
- set-o errexit
- pip install -r requirements.txt
- python manage.py collectstatic--no-input
- python manage.py migrate
+#!/usr/bin/env bash
+
+# Exit on error
+set -o errexit
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run database migrations
+python manage.py migrate
